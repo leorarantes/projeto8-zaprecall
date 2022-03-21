2f4cf52745598159c632a024547bc4ddc97d6d90
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-03-21 16:43:41 -0300

    deliver version

--- src/FlashcardsList.js
+++ src/FlashcardsList.js
@@ -1,22 +1,26 @@
 import Flashcard from "./Flashcard";
+import { useState } from "react";
 
 export default function FlashcardsList(props) {
+    const [flashcardStt, setFlashcardStt] = useState([1, 1, 1, 1, 1, 1, 1, 1]);
+
     const flashcardsArray = [
-        {flashcardState: 0, flashcardKey: 1, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 2, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 3, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 4, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 5, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 6, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 7, flashcardQuestion: "teste"},
-        {flashcardState: 0, flashcardKey: 8, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[0], flashcardKey: 1, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[1], flashcardKey: 2, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[2], flashcardKey: 3, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[3], flashcardKey: 4, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[4], flashcardKey: 5, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[5], flashcardKey: 6, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[6], flashcardKey: 7, flashcardQuestion: "teste"},
+        {flashcardState: flashcardStt[7], flashcardKey: 8, flashcardQuestion: "teste"},
     ];
 
         return (
             <ul className="flashcards-list">
                 {flashcardsArray.map(element => {
                     return (
-                    <Flashcard elementId={element} setDone={props.setDone} key={element.flashcardKey}/>);})}
+                        <Flashcard elementId={element} setDone={props.setDone} key={element.flashcardKey} done={props.done} setFlashcardStt={flashcardSttId => {setFlashcardStt(flashcardSttId)}} flashcardStt={flashcardStt} />
+                    );})}
             </ul>
         );
 }
--- src/Screen2.js
+++ src/Screen2.js
@@ -4,13 +4,12 @@ import Footer from "./Footer";
 import { useState } from "react";
 
 export default function Screen2(props) {
-    const [done, setDone] = useState(0);
-    console.log(done);
-    while(done !== 8) {
+    const [done, setDone] = useState([0,-1,-1,-1,-1,-1,-1,-1,-1]);
+    while(done[0] !== 8) {
         return (
             <div className="body2">
                 <Logo classId="logo2"/>
-                <FlashcardsList setDone={doneValue => {setDone(doneValue);}} />
+                <FlashcardsList setDone={doneValue => {setDone(doneValue);}} done={done} />
                 <Footer footerState="1" done={done} />
             </div>
         );
@@ -19,8 +18,8 @@ export default function Screen2(props) {
     return (
         <div className="body2">
             <Logo classId="logo2"/>
-            <FlashcardsList />
-            <Footer footerState="2"/>
+            <FlashcardsList done={done} />
+            <Footer footerState="2" done={done}/>
         </div>
     );
 }
